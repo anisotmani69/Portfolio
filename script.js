ed568b9e32eb416eab06863c6127dbe46a48ae7d
p2209754 <anisotmani96@gmail.com>
2024-06-18 15:08:14 +0200

    Projet Portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+function showInfo(projectId) {
+    document.getElementById(projectId).style.display = 'flex';
+}
+
+function hideInfo(projectId) {
+    document.getElementById(projectId).style.display = 'none';
+}
